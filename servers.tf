a589dedb5a662a39dc801a5fdabdbee47424cc3c
naveen p <naveenpendekatla0407@gmail.com>
2023-06-23 18:35:56 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -1,4 +1,5 @@
 data "aws_ami" "centos" {
+  most_recent = true
   owners = ["973714476881"]
 
 }
@@ -16,5 +17,77 @@ variable "instance_type" {
 
   tags = {
     Name = "frontend"
+  }
+}
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "user"
+  }
+}
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "redis"
+  }
+}
+resource "aws_instance" "mysql" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "mysql"
+  }
+}
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "shipping"
+  }
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos.image_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "payment"
   }
 }
