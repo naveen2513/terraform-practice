8f85d6da07811b478b84613290a63966bea1551e
naveen p <naveenpendekatla0407@gmail.com>
2023-06-23 18:58:59 +0530

    terraform route 53

--- servers.tf
+++ servers.tf
@@ -21,6 +21,13 @@ data "aws_security_group" "allow-all" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "frontend-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -30,6 +37,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "mongodb-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
@@ -41,6 +55,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "catalogue-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -50,6 +71,13 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "cart-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
@@ -61,6 +89,13 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "user-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
 resource "aws_instance" "redis" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -70,6 +105,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "redis-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
@@ -81,6 +123,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "mysql-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -90,6 +139,13 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "shipping-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
@@ -101,6 +157,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "rabbitmq-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -110,4 +173,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z09466133SH7C438NSMD2"
+  name    = "payment-dev.naveendevops2.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
