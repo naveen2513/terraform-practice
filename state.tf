93b08a0bb0885e98bd3368df233600ff6dbcd496
naveen p <naveenpendekatla0407@gmail.com>
2023-06-24 21:36:10 +0530

    terraform

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-2513"
-    key = "project/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform  {
+  #backend "s3" {
+    #bucket = "terraform-2513"
+   # key = "project/dev/terraform.tfstate"
+  #  region = "us-east-1"
+ # }
+#}
