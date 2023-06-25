9c0c97a9d182f2d89dcb1de50472efdda42e0e00
naveen p <naveenpendekatla0407@gmail.com>
2023-06-25 11:57:37 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -66,6 +66,10 @@ resource "aws_instance" "instance" {
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
+  tags = {
+    name = each.value["name"]
+  }
+
 }
 resource "aws_route53_record" "records" {
   for_each = var.components
