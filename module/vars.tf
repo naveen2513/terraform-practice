951ec161728b96d9a09b4cde5c1592cf6521c7d7
naveen p <naveenpendekatla0407@gmail.com>
2023-07-06 06:24:56 +0530

    terraform module

--- module/vars.tf
+++ module/vars.tf
@@ -2,6 +2,3 @@ variable "env" {}
 variable "instance_type" {}
 variable "component_name" {}
 variable "password" {}
-
-variable "database-servers" {}
-variable "app-servers" {}
--- servers.tf
+++ servers.tf
@@ -1,22 +1,10 @@
-module "database-servers" {
-  for_each = var.database-servers
+module "servers" {
+  for_each = var.components
 
   source = "./module"
   component_name = each.value["name"]
+  env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value, "password", "null" )
-  env = var.env
-  provisioner = true
-
-}
-module "app-servers" {
-  depends_on = [module.database-servers]
-  for_each = var.app-servers
-
-  source = "./module"
-  component_name = each.value["name"]
-  instance_type = each.value["instance_type"]
-  password = lookup(each.value, "password", "null" )
-  env = var.env
 
 }
