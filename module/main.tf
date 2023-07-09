733be0bb4895be7fb036a4b5a6c2bfe47b9247a8
naveen p <naveenpendekatla0407@gmail.com>
2023-07-09 07:24:21 +0530

    ansible  iam

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+#  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
 
   tags = {
@@ -32,8 +32,6 @@ resource "aws_route53_record" "records" {
 resource "aws_iam_role" "role" {
   name = "${var.component_name}-${var.env}-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -53,16 +51,16 @@ resource "aws_iam_role" "role" {
   }
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component_name}-${var.env}-role"
-  role = aws_iam_role.role.name
-}
+#resource "aws_iam_instance_profile" "instance_profile" {
+ # name = "${var.component_name}-${var.env}-role"
+  #role = aws_iam_role.role.name
+#}
 
 resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
 
-  policy = jsondecode({
+  policy =  jsondecode ({
     "Version": "2012-10-17",
     "Statement": [
       {
@@ -81,10 +79,9 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Effect": "Allow",
         "Action": "ssm:DescribeParameters",
         "Resource": "*"
-      }
+      },
     ]
   }
-
   )
 
   }
