2688564491f6e156adbec6eec9baa5a7dafac61e
naveen p <naveenpendekatla0407@gmail.com>
2023-07-09 07:02:40 +0530

    ansible  iam

--- module/main.tf
+++ module/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
 
   tags = {
@@ -28,7 +29,7 @@ resource "aws_route53_record" "records" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
-resource "aws_iam_role" "test_role" {
+resource "aws_iam_role" "role" {
   name = "${var.component_name}-${var.env}-role"
 
   # Terraform's "jsonencode" function converts a
@@ -52,9 +53,14 @@ resource "aws_iam_role" "test_role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component_name}-${var.env}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.test_role
+  role = aws_iam_role.role.id
 
   policy = jsondecode({
     "Version": "2012-10-17",
