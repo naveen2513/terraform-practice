e77dd3f90c3353fd972a303930263d1df81d70f0
naveen p <naveenpendekatla0407@gmail.com>
2023-07-11 08:22:40 +0530

    ansible  kms

--- module/main.tf
+++ module/main.tf
@@ -67,12 +67,16 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
         "Sid": "VisualEditor0",
         "Effect": "Allow",
         "Action": [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": "arn:aws:ssm:us-east-1:884604107698:parameter/${var.env}.${var.component_name}.*"
+        "Resource": [
+          "arn:aws:ssm:us-east-1:884604107698:parameter/${var.env}.${var.component_name}.*",
+          "arn:aws:kms:us-east-1:884604107698:key/4cc4aa7a-e873-409c-bbe8-fc4b8a80bc2c"
+        ]
       },
       {
         "Sid": "VisualEditor1",
