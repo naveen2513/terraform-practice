d3aff0381838f712ad67f9b41d9605b3d24a0eda
naveen p <naveenpendekatla0407@gmail.com>
2023-07-23 23:03:41 +0530

    ansible  kms

--- module/main.tf
+++ module/main.tf
@@ -33,7 +33,7 @@ resource "aws_route53_record" "records" {
   records = [aws_instance.instance.private_ip]
 }
 resource "aws_iam_role" "role" {
-  name = "${var.component_name}-${var.env}-role"
+  name = "${var.component_name}.${var.env}-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -50,17 +50,17 @@ resource "aws_iam_role" "role" {
   })
 
   tags = {
-    tag-key = "${var.component_name}-${var.env}-role"
+    tag-key = "${var.component_name}.${var.env}-role"
   }
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component_name}-${var.env}-role"
+  name = "${var.component_name}.${var.env}-role"
   role = aws_iam_role.role.name
 }
 
 resource "aws_iam_role_policy" "ssm-ps-policy" {
-  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  name = "${var.component_name}.${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
 
   policy = jsonencode({
@@ -77,7 +77,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
           "ssm:GetParameter"
         ],
         "Resource": [
-          "arn:aws:ssm:us-east-1:884604107698:parameter/${var.env}-${var.component_name}.*",
+          "arn:aws:ssm:us-east-1:884604107698:parameter/${var.env}.${var.component_name}.*",
           "arn:aws:kms:us-east-1:884604107698:key/4cc4aa7a-e873-409c-bbe8-fc4b8a80bc2c"
         ]
       },
