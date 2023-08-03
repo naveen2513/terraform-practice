52665ca08d443f828e12406e9ca6da9624534cec
naveen p <naveenpendekatla0407@gmail.com>
2023-08-03 15:58:30 +0530

    vpc tags

--- main.tf
+++ main.tf
@@ -2,4 +2,6 @@ module "vpc" {
   source = "git::https://github.com/naveen2513/tf-module-vpc.git"
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
+  tags   = local.tags
+  env = var.env
 }
---
+++ tags.tf
@@ -0,0 +1,9 @@
+local = {
+  tags = {
+    business_unit = "ecommerce"
+    business_type = "retail"
+    project = "roboshop"
+    cost_center = 100
+    env = var.env
+  }
+}
